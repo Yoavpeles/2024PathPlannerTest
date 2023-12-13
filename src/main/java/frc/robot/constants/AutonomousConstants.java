f6721aa8616c9cecd006dffc33ffe88a062148ac
yoavpeles <yoav.peles@gmail.com>
2023-12-13 15:33:40 +0200

    added constants

---
+++ src/main/java/frc/robot/constants/AutonomousConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+public class AutonomousConstants {
+    // TODO: update speeds
+    public static final double kMaxSpeedMetersPerSecond = 2.0;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 2.0;
+
+    public static final String defaultPath = "bot-3-gp-engage";
+
+    // TODO: update P gains
+    public static final double kPTranslationController = 5;
+    public static final double kPThetaController = 5;
+
+    public static final double engageStartSpeedMetersPerSecond = 1.0;
+
+    public static final double engageSpeedMetersPerSecond = 0.4;
+}
