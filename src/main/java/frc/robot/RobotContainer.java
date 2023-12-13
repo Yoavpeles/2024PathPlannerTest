bb6a2053c6c99026ecf16b3aa22477cbd892baea
yoavpeles <yoav.peles@gmail.com>
2023-12-14 01:05:16 +0200

    configed the drivetrain,autoFactory,autochooser (sorry for the messy commit)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,19 +4,38 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 
-
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.autonomous.AutoFactory;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class RobotContainer {
+  Drivetrain m_drivetrain = new Drivetrain();
+  private final SendableChooser<Command> chooser;
+  private final AutoFactory _autoFactory = new AutoFactory(m_drivetrain);
+  
   public RobotContainer() {
+    chooser = AutoBuilder.buildAutoChooser();
+    initChooser();
+
     // Configure the trigger bindings
     configureBindings();
   }
 
-  
   private void configureBindings() {
-    
+
   }
 
- 
+  private void initChooser() {
+     SmartDashboard.putData("Auto Chooser", chooser);
+    chooser.addOption("testauto",_autoFactory.createAuto("testauto") );
+  }
+
+  public Command getAutonomousCommand() {
+    return chooser.getSelected();
+}
+
 }
