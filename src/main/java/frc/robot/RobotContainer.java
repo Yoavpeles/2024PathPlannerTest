d899591d329db4887aec45ab1f8c6de64fdf38b0
yoavpeles <yoav.peles@gmail.com>
2023-12-14 11:22:02 +0200

    added drive controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,22 +5,36 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.autonomous.AutoFactory;
+import frc.robot.constants.DrivetrainConstants;
+import frc.robot.constants.DrivetrainConstants.SwerveModuleConstants;
+import frc.robot.humanIO.CommandPS5Controller;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class RobotContainer {
+  private final CommandPS5Controller _driverController = new CommandPS5Controller(0);
   Drivetrain m_drivetrain = new Drivetrain();
   private final SendableChooser<Command> chooser;
   private final AutoFactory _autoFactory = new AutoFactory(m_drivetrain);
   
   public RobotContainer() {
+ m_drivetrain.setDefaultCommand(new RunCommand(() -> m_drivetrain.drive(
+                _driverController.getLeftY() *
+                        SwerveModuleConstants.freeSpeedMetersPerSecond,
+                _driverController.getLeftX() *
+                        SwerveModuleConstants.freeSpeedMetersPerSecond,
+                _driverController.getCombinedAxis() *
+                        DrivetrainConstants.maxRotationSpeedRadPerSec,
+                false), m_drivetrain));
+    
     chooser = AutoBuilder.buildAutoChooser();
     initChooser();
 
+    //PPLibTelemetry();
     // Configure the trigger bindings
     configureBindings();
   }
