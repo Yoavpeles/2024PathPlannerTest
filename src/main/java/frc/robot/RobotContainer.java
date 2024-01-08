1abb7e7a8f2973396e1923fbe6daeb388c58bef7
yoavpeles <yoav.peles@gmail.com>
2024-01-08 16:27:22 +0200

    added back the getAutonomousCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
    public RobotContainer() {
@@ -14,6 +15,12 @@ public class RobotContainer {
  
   
   private void configureBindings() {}
+
+
+
+public Command getAutonomousCommand() {
+    return null;
+}
 
   
 }
