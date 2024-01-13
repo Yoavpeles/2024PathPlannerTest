9156ed56ac1d94ea82b910b21bf7dc81b82ea197
yoavpeles <yoav.peles@gmail.com>
2024-01-13 10:26:41 +0200

    removed unuesd imports

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.drivetrain;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
